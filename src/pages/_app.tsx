362f22456629c9be412fe5a5753b2b14dbe61635
Jasper Mayone <me@jaspermayone.com>
2024-08-16 11:19:24 -0400

    meta improvements

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,41 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#ffffff" />
+        <title>Metro</title>
+        <meta
+          name="description"
+          content="An interactive map of Boston's transit system."
+        />
+        <meta
+          name="keywords"
+          content="mbta boston transit Boston transportation Boston subway MBTA map Boston public transit Massachusetts transit Boston metro Boston bus routes MBTA schedules Boston train map Massachusetts Bay Transportation Authority Boston commute Boston subway map MBTA guide Boston transit app Boston T map"
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metro.jaspermayone.com" />
+        <meta property="og:title" content="Metro" />
+        <meta
+          property="og:description"
+          content="An interactive map of Boston's transit system."
+        />
+        <meta property="og:image" content="/MBTA.svg" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metro.jaspermayone.com" />
+        <meta property="twitter:title" content="Metro" />
+        <meta
+          property="twitter:description"
+          content="An interactive map of Boston's transit system."
+        />
+        <meta property="twitter:image" content="/MBTA.svg" />
+        {/* */}
+        <meta name="author" content="Jasper Mayone" />
+        <meta name="robots" content="index, follow" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="msapplication-TileColor" content="#ffffff" />
